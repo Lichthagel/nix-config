4845764020f56fe37d8de3a980fc5e662fe47941
Lichthagel <hey@lichthagel.de>
2023-11-28 12:23:25 +0100

    feat(zsh): update plugins

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -1,28 +1,70 @@
-{
+{pkgs, ...}: let
+  catppuccin-zsh-fsh = pkgs.stdenvNoCC.mkDerivation {
+    name = "catppuccin-zsh-fsh";
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "zsh-fsh";
+      rev = "7cdab58bddafe0565f84f6eaf2d7dd109bd6fc18";
+      sha256 = "sha256-31lh+LpXGe7BMZBhRWvvbOTkwjOM77FPNaGy6d26hIA=";
+    };
+    phases = ["buildPhase"];
+    buildPhase = ''
+      mkdir -p $out/share/zsh/site-functions/themes
+      ls $src/themes
+      cp $src/themes/* $out/share/zsh/site-functions/themes/
+    '';
+  };
+  ohmyzsh-src = pkgs.fetchFromGitHub {
+    owner = "ohmyzsh";
+    repo = "ohmyzsh";
+    rev = "7e3fdf33ec53821e574b8d6df43bec7264c507ee";
+    sha256 = "sha256-GYM0VIIFdJJOV6Nu3ZuaiNkTlbuLOs7cX9665oVsTq4=";
+  };
+in {
   programs.zsh = {
     enable = true;
+    dotDir = ".config/zsh";
     enableAutosuggestions = true;
     enableCompletion = true;
     defaultKeymap = "emacs";
     shellAliases = {
       cat = "bat";
     };
-    antidote = {
-      enable = true;
-      plugins = [
-        "MichaelAquilina/zsh-you-should-use"
-        "zdharma-continuum/fast-syntax-highlighting"
-        "chrissicool/zsh-256color"
-        "belak/zsh-utils path:editor"
-        "ohmyzsh/ohmyzsh path:plugins/git-commit"
-        "ohmyzsh/ohmyzsh path:plugins/gitfast"
-        "ohmyzsh/ohmyzsh path:plugins/npm kind:defer"
-        "ohmyzsh/ohmyzsh path:plugins/nvm"
-        "ohmyzsh/ohmyzsh path:plugins/pip kind:defer"
-        "ohmyzsh/ohmyzsh path:plugins/pipenv"
-        "ohmyzsh/ohmyzsh path:plugins/podman"
-        "ohmyzsh/ohmyzsh path:plugins/pylint"
-      ];
-    };
+    plugins = [
+      {
+        name = "zsh-you-should-use";
+        src = pkgs.zsh-you-should-use;
+        file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
+      }
+      {
+        name = "zsh-nix-shell";
+        src = pkgs.zsh-nix-shell;
+        file = "share/zsh-nix-shell/nix-shell.plugin.zsh";
+      }
+      {
+        name = "zsh-fast-syntax-highlighting";
+        src = pkgs.zsh-fast-syntax-highlighting;
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+      }
+      {
+        name = "git-commit";
+        src = ohmyzsh-src;
+        file = "plugins/git-commit/git-commit.plugin.zsh";
+      }
+      {
+        name = "gitfast";
+        src = ohmyzsh-src;
+        file = "plugins/gitfast/gitfast.plugin.zsh";
+      }
+      {
+        name = "podman";
+        src = ohmyzsh-src;
+        file = "plugins/podman/podman.plugin.zsh";
+      }
+    ];
+  };
+
+  xdg.configFile = {
+    "fsh".source = "${catppuccin-zsh-fsh}/share/zsh/site-functions/themes";
   };
 }
