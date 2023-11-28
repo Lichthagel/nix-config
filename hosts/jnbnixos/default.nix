7292692ab02e263a7360cf7e63ecf19818d1236b
Lichthagel <hey@lichthagel.de>
2023-11-28 12:23:45 +0100

    feat(podman): install

--- hosts/jnbnixos/default.nix
+++ hosts/jnbnixos/default.nix
@@ -103,6 +103,14 @@
     pkgs.helix
   ];
 
+  virtualisation.podman = {
+    enable = true;
+
+    dockerCompat = true;
+
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
