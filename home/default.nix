09db6cd90e39cb7b0f109cb4db500a5335bbb5e9
Lichthagel <hey@lichthagel.de>
2023-11-30 12:55:52 +0100

    feat(spotify): install

--- home/default.nix
+++ home/default.nix
@@ -28,6 +28,7 @@
     cascadia-code
     yubioath-flutter
     tutanota-desktop
+    spotify
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
