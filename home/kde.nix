19dabb01bf89bde587f701ad61bb77d658b17e76
Lichthagel <hey@lichthagel.de>
2023-12-02 01:18:31 +0100

    feat(kde): vimix cursors

--- home/kde.nix
+++ home/kde.nix
@@ -2,7 +2,48 @@
   pkgs,
   ctp,
   ...
-}: {
+}: let
+  vimix-cursors = pkgs.stdenvNoCC.mkDerivation {
+    name = "vimix-cursors";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "vinceliuice";
+      repo = "vimix-cursors";
+      rev = "2020-02-24";
+      sha256 = "sha256-TfcDer85+UOtDMJVZJQr81dDy4ekjYgEvH1RE1IHMi4=";
+    };
+
+    # not needed since the repo includes prebuilt cursors
+    # but kept here for reference
+
+    # nativeBuildInputs = with pkgs; [
+    #   (python3.withPackages (ps: with ps; [cairosvg]))
+    #   xorg.xcursorgen
+    # ];
+
+    # postPatch = ''
+    #   patchShebangs .
+    # '';
+
+    # buildPhase = ''
+    #   runHook preBuild
+
+    #   HOME="$NIX_BUILD_ROOT" ./build.sh
+
+    #   runHook postBuild
+    # '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/icons
+      cp -r dist $out/share/icons/Vimix-cursors
+      cp -r dist-white $out/share/icons/Vimix-white-cursors
+
+      runHook postInstall
+    '';
+  };
+in {
   home.packages = [
     (pkgs.catppuccin-kde.override {
       flavour = [ctp.flavor];
@@ -13,6 +54,7 @@
       inherit (ctp) flavor accent;
     })
     pkgs.capitaine-cursors
+    vimix-cursors
     (pkgs.catppuccin-gtk.override {
       variant = ctp.flavor;
       accents = [ctp.accent];
@@ -20,9 +62,9 @@
   ];
 
   home.pointerCursor = {
-    name = "Capitaine Cursors - White";
+    name = "Vimix Cursors - White";
     # size = 32;
-    package = pkgs.capitaine-cursors;
+    package = vimix-cursors;
     gtk.enable = true;
     x11.enable = true;
   };
