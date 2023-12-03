d90ef3d5a967873a73ed7588ced0b0dc19a4d33b
Lichthagel <hey@lichthagel.de>
2023-12-03 20:35:22 +0100

    feat(thunderbird): install

--- home/default.nix
+++ home/default.nix
@@ -30,6 +30,7 @@
     tutanota-desktop
     spotify
     obsidian
+    thunderbird
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
