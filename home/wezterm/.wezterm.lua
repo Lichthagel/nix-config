19cdc9b25c0b35870a2b48a5ca868f897ecc7bf8
Lichthagel <hey@lichthagel.de>
2023-12-09 12:06:03 +0100

    feat(wezterm): update font

--- home/wezterm/.wezterm.lua
+++ home/wezterm/.wezterm.lua
@@ -16,14 +16,16 @@ end
 config.color_scheme = 'Catppuccin Mocha'
 
 config.font = wezterm.font_with_fallback({
-  "Cascadia Code",
+  { family = "Cascadia Code", harfbuzz_features = { "calt=1", "ss01=1" } },
+  -- { family = "Fira Code", harfbuzz_features = { "zero=1", "cv14=1", "ss04=1", "ss06" } },
+  -- { family = "JetBrains Mono", harfbuzz_features = { "cv14=1" } },
+  -- { family = "Lilex", harfbuzz_features = { "cv09=1", "ss03=1" } },
+  -- { family = "MonoLisa", harfbuzz_features = { "ss02=1", "ss04=1", "ss11=1", "ss12=1" } },
   "Symbols Nerd Font",
 })
 
--- config.font_size = 12.0
+config.font_size = 11.0
 -- config.front_end = "WebGpu"
-
-config.harfbuzz_features = { "calt=1", "ss01=1" }
 
 config.use_fancy_tab_bar = false
 
