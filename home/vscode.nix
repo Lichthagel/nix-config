b44c2ba367bdfa45fa4ef33d22f2c68b79b8c0f6
Lichthagel <hey@lichthagel.de>
2023-12-13 20:39:57 +0100

    fix(vscode): only override x86_64-linux package

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,36 +1,26 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  flake-utils,
+  ...
+}: {
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
     package =
-      (pkgs.vscode.overrideAttrs rec {
-        version = "1.85.1";
+      # I don't really use other systems, so I only override the x86_64-linux package.
+      if pkgs.system == flake-utils.lib.system.x86_64-linux
+      then
+        (pkgs.vscode.overrideAttrs rec {
+          version = "1.85.1";
 
-        src = pkgs.fetchurl (
-          let
-            plat =
-              {
-                x86_64-linux = "linux-x64";
-                x86_64-darwin = "darwin";
-                aarch64-linux = "linux-arm64";
-                aarch64-darwin = "darwin-arm64";
-                armv7l-linux = "linux-armhf";
-              }
-              .${pkgs.system}
-              or pkgs.throwSystem;
-
-            archive_fmt =
-              if pkgs.stdenv.isDarwin
-              then "zip"
-              else "tar.gz";
-          in {
-            name = "VSCode_${version}_${plat}.${archive_fmt}";
-            url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+          src = pkgs.fetchurl {
+            name = "VSCode_${version}_linux-x64.tar.gz";
+            url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
             sha256 = "sha256-AWadaeVnpSkDNrHS97Lw8YFunXCZAEuBom+PQO4Xyfw=";
-          }
-        );
-      })
-      .fhs;
+          };
+        })
+        .fhs
+      else pkgs.vscode.fhs;
   };
 
   home.packages = [
