e543f361d785a1346bf24700d32a5bfa8b7c293f
Lichthagel <hey@lichthagel.de>
2023-12-13 13:16:17 +0100

    feat(vscode): update to 1.85.0

--- home/vscode.nix
+++ home/vscode.nix
@@ -2,7 +2,35 @@
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
-    package = pkgs.vscode-fhs;
+    package =
+      (pkgs.vscode.overrideAttrs rec {
+        version = "1.85.0";
+
+        src = pkgs.fetchurl (
+          let
+            plat =
+              {
+                x86_64-linux = "linux-x64";
+                x86_64-darwin = "darwin";
+                aarch64-linux = "linux-arm64";
+                aarch64-darwin = "darwin-arm64";
+                armv7l-linux = "linux-armhf";
+              }
+              .${pkgs.system}
+              or pkgs.throwSystem;
+
+            archive_fmt =
+              if pkgs.stdenv.isDarwin
+              then "zip"
+              else "tar.gz";
+          in {
+            name = "VSCode_${version}_${plat}.${archive_fmt}";
+            url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+            sha256 = "sha256-ri5t3CLDLn+GX6oGoA0XEZQdzx2ykHlMbtZZ1aFBOXc";
+          }
+        );
+      })
+      .fhs;
   };
 
   home.packages = [
