9a6f986c3838b21d16660f51fb1bba560cb00f3f
Lichthagel <hey@lichthagel.de>
2023-12-17 09:57:15 +0100

    fix(ime): enable wayland ime in electron apps

--- home/discord.nix
+++ home/discord.nix
@@ -5,9 +5,17 @@
   ...
 }: {
   home.packages = with pkgs; [
-    (discord.override {
-      withOpenASAR = true;
-      withVencord = true;
-    })
+    ((discord.override {
+        withOpenASAR = true;
+        withVencord = true;
+      })
+      .overrideAttrs (oldAttrs:
+        lib.optionalAttrs (config.i18n.inputMethod == "fcitx5") {
+          installPhase =
+            builtins.replaceStrings
+            [''--add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}"'']
+            [''--add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" --add-flags --enable-wayland-ime'']
+            oldAttrs.installPhase;
+        }))
   ];
 }
--- home/vscode.nix
+++ home/vscode.nix
@@ -1,4 +1,6 @@
 {
+  config,
+  lib,
   pkgs,
   flake-utils,
   ...
@@ -10,15 +12,21 @@
       # I don't really use other systems, so I only override the x86_64-linux package.
       if pkgs.system == flake-utils.lib.system.x86_64-linux
       then
-        (pkgs.vscode.overrideAttrs rec {
-          version = "1.85.1";
+        ((pkgs.vscode.override
+            (lib.optionalAttrs (config.i18n.inputMethod == "fcitx5") {
+              commandLineArgs = [
+                "--enable-wayland-ime"
+              ];
+            }))
+          .overrideAttrs rec {
+            version = "1.85.1";
 
-          src = pkgs.fetchurl {
-            name = "VSCode_${version}_linux-x64.tar.gz";
-            url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
-            sha256 = "sha256-AWadaeVnpSkDNrHS97Lw8YFunXCZAEuBom+PQO4Xyfw=";
-          };
-        })
+            src = pkgs.fetchurl {
+              name = "VSCode_${version}_linux-x64.tar.gz";
+              url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
+              sha256 = "sha256-AWadaeVnpSkDNrHS97Lw8YFunXCZAEuBom+PQO4Xyfw=";
+            };
+          })
         .fhs
       else pkgs.vscode.fhs;
   };
