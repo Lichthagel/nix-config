9860c234a26930080adac370c91a56dcbba49217
Lichthagel <hey@lichthagel.de>
2024-01-20 11:38:20 +0100

    Update version and sha256 in monolisa.nix

--- packages/monolisa.nix
+++ packages/monolisa.nix
@@ -1,12 +1,12 @@
 {pkgs ? import <nixpkgs> {}, ...}:
 pkgs.stdenvNoCC.mkDerivation rec {
   pname = "monolisa";
-  version = "2.011";
+  version = "2.012";
 
   src = pkgs.requireFile {
     name = "MonoLisa-Plus-${version}.zip";
     url = "https://www.monolisa.dev/orders";
-    sha256 = "sha256:6de35b7dfcb8dfb67a509888d7d70d7aa92bc489d7b05d7184cb80b5e8a5043f";
+    sha256 = "sha256:a241f30e49f373f672a99271e25923df2eedd51e1ebab33fc14dc451972274ab";
   };
 
   nativeBuildInputs = with pkgs; [
