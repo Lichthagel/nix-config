7da9d2195e14b5d93610362ae2560219a82400ef
Lichthagel <hey@lichthagel.de>
2024-01-28 15:49:46 +0100

    Update accent color to teal

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
     ctp = {
       flavor = "mocha";
       flavorCapitalized = "Mocha";
-      accent = "pink";
-      accentCapitalized = "Pink";
+      accent = "teal";
+      accentCapitalized = "Teal";
     };
   in
     flake-parts.lib.mkFlake {inherit inputs;}
