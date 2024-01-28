78580d3d2a074b0cdf5772a840d12401ea73c0c5
Lichthagel <hey@lichthagel.de>
2024-01-28 15:50:23 +0100

    Update font configuration in .wezterm.lua

--- home/wezterm/.wezterm.lua
+++ home/wezterm/.wezterm.lua
@@ -20,11 +20,12 @@ config.font = wezterm.font_with_fallback({
   -- { family = "Fira Code", harfbuzz_features = { "zero=1", "cv14=1", "ss04=1", "ss06" } },
   -- { family = "JetBrains Mono", harfbuzz_features = { "cv14=1" } },
   -- { family = "Lilex", harfbuzz_features = { "cv09=1", "ss03=1" } },
-  -- { family = "MonoLisa", harfbuzz_features = { "ss02=1", "ss04=1", "ss11=1", "ss12=1" } },
+  -- { family = "MonoLisa", harfbuzz_features = { "ss02=1", "ss04=1", "ss08=1", "ss11=1", "ss12=1" } },
   "Symbols Nerd Font",
 })
 
 config.font_size = 11.0
+-- config.line_height = 0.9
 -- config.front_end = "WebGpu"
 
 wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(config, {
