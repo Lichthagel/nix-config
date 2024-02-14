af53909c3e2f7d21ac95146b2a7261a723d3be8f
Lichthagel <hey@lichthagel.de>
2024-02-14 22:32:33 +0100

    add swap on desktop

--- hosts/jdnixos/hardware-configuration.nix
+++ hosts/jdnixos/hardware-configuration.nix
@@ -42,7 +42,12 @@
     options = ["rw" "uid=1000" "gid=1000" "umask=022" "fmask=133" "dmask=022" "prealloc" "windows_names"];
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
