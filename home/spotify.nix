f7cb59f9dcf85f4922882d8aa657c5bdfd9a19bd
Lichthagel <hey@lichthagel.de>
2024-02-20 11:16:30 +0100

    update catppuccin-spicetify

--- home/spotify.nix
+++ home/spotify.nix
@@ -20,7 +20,16 @@ in {
   programs.spicetify = {
     enable = true;
 
-    theme = spicePkgs.themes.catppuccin;
+    theme =
+      spicePkgs.themes.catppuccin
+      // {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "spicetify";
+          rev = "d3c4b697f1739149684e36977a1502f88c344b3a";
+          sha256 = "sha256-VxkgW9qF1pmKnP7Ei7gobF0jVB1+qncfFeykWoXMRCo=";
+        };
+      };
     colorScheme = ctp.flavor;
   };
 }
