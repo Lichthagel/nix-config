a589424268d20e137f222f07f64065f64b981814
Lichthagel <hey@lichthagel.de>
2024-02-27 18:03:33 +0100

    home: install zoom-us

--- home/default.nix
+++ home/default.nix
@@ -38,6 +38,7 @@
           oldAttrs.installPhase;
       }))
     thunderbird
+    zoom-us
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
