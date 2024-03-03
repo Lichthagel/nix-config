3f352279c6ae63325b42af0ff8802065f49fdf3e
Lichthagel <hey@lichthagel.de>
2024-03-03 22:25:58 +0100

    fix garbage collection

--- nixos/base.nix
+++ nixos/base.nix
@@ -63,7 +63,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 1w";
+      options = "--delete-older-than 7d";
     };
     settings = (import ../flake.nix).nixConfig;
     registry = {
