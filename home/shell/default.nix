8183ea5bb1d2c688b0008dc3dfa7881c245ce4c8
Lichthagel <hey@lichthagel.de>
2024-03-03 17:58:54 +0100

    update catppuccin/bat

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -28,8 +28,8 @@
         ctpsrc = pkgs.fetchFromGitHub {
           owner = "catppuccin";
           repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          rev = "0ce3d34921ba1b544a4d82aa01352abd553d51ef";
+          sha256 = "sha256-PLbTLj0qhsDj+xm+OML/AQsfRQVPXLYQNEPllgKcEx4=";
         };
       in {
         "catppuccin-${ctp.flavor}" = {
