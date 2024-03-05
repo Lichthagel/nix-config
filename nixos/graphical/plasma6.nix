37b47329438d19c074f58d54d9f9ad4915125e45
Lichthagel <hey@lichthagel.de>
2024-03-05 23:16:20 +0100

    Add excludePackages to plasma6 environment

--- nixos/graphical/plasma6.nix
+++ nixos/graphical/plasma6.nix
@@ -1,3 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   services.xserver.desktopManager.plasma6.enable = true;
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    elisa
+    kate
+    khelpcenter
+    kwrited
+    plasma-browser-integration
+  ];
 }
