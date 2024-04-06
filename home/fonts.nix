be6d836138219729fa757ef59a6f7c159f8926c3
Lichthagel <hey@lichthagel.de>
2024-04-06 22:58:25 +0200

    home/fonts: enable is os is graphical

--- home/fonts.nix
+++ home/fonts.nix
@@ -1,80 +1,84 @@
 {
+  osConfig,
+  lib,
   pkgs,
   selfPkgs,
   unstablePkgs,
   ...
 }:
 {
-  home.packages = with pkgs; [
-    # sans & mono
-    unstablePkgs.geist-font
+  config = lib.mkIf osConfig.licht.graphical.enable {
+    home.packages = with pkgs; [
+      # sans & mono
+      unstablePkgs.geist-font
 
-    # sans
-    (google-fonts.override {
-      fonts = [
-        "Josefin Sans"
-        "Josefin Slab"
-        "M PLUS 1"
-        "M PLUS 1 Code"
-        "M PLUS 2"
-        "Outfit"
-        "Plus Jakarta Sans"
-        "Rubik"
-        "Sen"
-        "Sora"
-      ];
-    })
-    selfPkgs.afacad
-    selfPkgs.gabarito
-    inter
-    jost
-    lexend
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    noto-fonts-monochrome-emoji
-    overpass
+      # sans
+      (google-fonts.override {
+        fonts = [
+          "Josefin Sans"
+          "Josefin Slab"
+          "M PLUS 1"
+          "M PLUS 1 Code"
+          "M PLUS 2"
+          "Outfit"
+          "Plus Jakarta Sans"
+          "Rubik"
+          "Sen"
+          "Sora"
+        ];
+      })
+      selfPkgs.afacad
+      selfPkgs.gabarito
+      inter
+      jost
+      lexend
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      noto-fonts-monochrome-emoji
+      overpass
 
-    # mono
-    (unstablePkgs.nerdfonts.override {
-      fonts = [
-        "CascadiaCode"
-        "FantasqueSansMono"
-        "FiraCode"
-        "GeistMono"
-        # "Iosevka"
-        "JetBrainsMono"
-        "MartianMono"
-        "Monaspace"
-        "NerdFontsSymbolsOnly"
-        "SourceCodePro"
-        # "VictorMono"
-      ];
-    })
-    cascadia-code
-    selfPkgs.cartograph-cf
-    selfPkgs.cartograph-cf-nerdfont
-    fantasque-sans-mono
-    fira-code
-    # iosevka-bin
-    jetbrains-mono
-    unstablePkgs.kode-mono
-    selfPkgs.kode-mono-nerdfont
-    selfPkgs.lilex
-    martian-mono
-    monaspace
-    selfPkgs.monolisa
-    selfPkgs.monolisa-custom
-    selfPkgs.monolisa-nerdfont
-    selfPkgs.monolisa-custom-nerdfont
-    # recursive
-    # selfPkgs.recursive-nerdfont
-    source-code-pro
-    selfPkgs.twilio-sans-mono
-    selfPkgs.twilio-sans-mono-nerdfont
-    # victor-mono
-  ];
+      # mono
+      (unstablePkgs.nerdfonts.override {
+        fonts = [
+          "CascadiaCode"
+          "FantasqueSansMono"
+          "FiraCode"
+          "GeistMono"
+          # "Iosevka"
+          "JetBrainsMono"
+          "MartianMono"
+          "Monaspace"
+          "NerdFontsSymbolsOnly"
+          "SourceCodePro"
+          # "VictorMono"
+        ];
+      })
+      cascadia-code
+      selfPkgs.cartograph-cf
+      selfPkgs.cartograph-cf-nerdfont
+      fantasque-sans-mono
+      fira-code
+      # iosevka-bin
+      jetbrains-mono
+      unstablePkgs.kode-mono
+      selfPkgs.kode-mono-nerdfont
+      selfPkgs.lilex
+      martian-mono
+      monaspace
+      selfPkgs.monolisa
+      selfPkgs.monolisa-custom
+      selfPkgs.monolisa-nerdfont
+      selfPkgs.monolisa-custom-nerdfont
+      # recursive
+      # selfPkgs.recursive-nerdfont
+      source-code-pro
+      selfPkgs.twilio-sans-mono
+      selfPkgs.twilio-sans-mono-nerdfont
+      # victor-mono
+    ];
 
-  fonts.fontconfig.enable = true;
+    fonts.fontconfig.enable = true;
+  };
 }
