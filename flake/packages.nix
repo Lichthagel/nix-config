6f70fa818815fb6968ee5957b233b579449195ee
Lichthagel <hey@lichthagel.de>
2024-04-06 16:51:53 +0200

    replace strings with paths in flake/packages

--- flake/packages.nix
+++ flake/packages.nix
@@ -4,17 +4,17 @@
     { pkgs, unstablePkgs, ... }:
     {
       packages = rec {
-        afacad = pkgs.callPackage "${self}/packages/afacad.nix" { };
-        cartograph-cf = pkgs.callPackage "${self}/packages/cartograph-cf.nix" { };
-        cartograph-cf-nerdfont = pkgs.callPackage "${self}/packages/nerdfont.nix" { font = cartograph-cf; };
-        catppuccin-fcitx5 = pkgs.callPackage "${self}/packages/catppuccin-fcitx5.nix" { };
-        catppuccin-sddm = pkgs.callPackage "${self}/packages/catppuccin-sddm.nix" { };
-        gabarito = pkgs.callPackage "${self}/packages/gabarito.nix" { };
-        kode-mono-nerdfont = pkgs.callPackage "${self}/packages/nerdfont.nix" {
+        afacad = pkgs.callPackage (self + /packages/afacad.nix) { };
+        cartograph-cf = pkgs.callPackage (self + /packages/cartograph-cf.nix) { };
+        cartograph-cf-nerdfont = pkgs.callPackage (self + /packages/nerdfont.nix) { font = cartograph-cf; };
+        catppuccin-fcitx5 = pkgs.callPackage (self + /packages/catppuccin-fcitx5.nix) { };
+        catppuccin-sddm = pkgs.callPackage (self + /packages/catppuccin-sddm.nix) { };
+        gabarito = pkgs.callPackage (self + /packages/gabarito.nix) { };
+        kode-mono-nerdfont = pkgs.callPackage (self + /packages/nerdfont.nix) {
           font = unstablePkgs.kode-mono;
         };
-        lilex = pkgs.callPackage "${self}/packages/lilex.nix" { };
-        monolisa = pkgs.callPackage "${self}/packages/monolisa.nix" { };
+        lilex = pkgs.callPackage (self + /packages/lilex.nix) { };
+        monolisa = pkgs.callPackage (self + /packages/monolisa.nix) { };
         monolisa-custom = monolisa.overrideAttrs (oldAttrs: {
           pname = "monolisa-custom";
 
@@ -25,13 +25,13 @@
             sha256 = "sha256:0ilvvzg709l60l1cwdih729n71kdl83waaa23as94mnzln99z1rf";
           };
         });
-        monolisa-nerdfont = pkgs.callPackage "${self}/packages/nerdfont.nix" { font = monolisa; };
-        monolisa-custom-nerdfont = pkgs.callPackage "${self}/packages/nerdfont.nix" {
+        monolisa-nerdfont = pkgs.callPackage (self + /packages/nerdfont.nix) { font = monolisa; };
+        monolisa-custom-nerdfont = pkgs.callPackage (self + /packages/nerdfont.nix) {
           font = monolisa-custom;
         };
-        recursive-nerdfont = pkgs.callPackage "${self}/packages/nerdfont.nix" { font = pkgs.recursive; };
-        twilio-sans-mono = pkgs.callPackage "${self}/packages/twilio-sans-mono.nix" { };
-        twilio-sans-mono-nerdfont = pkgs.callPackage "${self}/packages/nerdfont.nix" {
+        recursive-nerdfont = pkgs.callPackage (self + /packages/nerdfont.nix) { font = pkgs.recursive; };
+        twilio-sans-mono = pkgs.callPackage (self + /packages/twilio-sans-mono.nix) { };
+        twilio-sans-mono-nerdfont = pkgs.callPackage (self + /packages/nerdfont.nix) {
           font = twilio-sans-mono;
         };
       };
