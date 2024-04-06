af775ce9f4eb985609a49dc3a3b233a0277821b7
Lichthagel <hey@lichthagel.de>
2024-04-06 22:10:15 +0200

    home/spotify: enable per option

--- home/spotify.nix
+++ home/spotify.nix
@@ -1,26 +1,39 @@
 {
+  config,
+  lib,
   ctp,
   pkgs,
   inputs,
   ...
 }:
 let
-  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+  cfg = config.licht.media.spotify;
 in
 {
   imports = [ inputs.spicetify-nix.homeManagerModule ];
 
-  programs.spicetify = {
-    enable = true;
+  options.licht.media.spotify = {
+    enable = lib.mkEnableOption "spotify";
+  };
 
-    theme = spicePkgs.themes.catppuccin // {
-      src = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "spicetify";
-        rev = "d3c4b697f1739149684e36977a1502f88c344b3a";
-        sha256 = "sha256-VxkgW9qF1pmKnP7Ei7gobF0jVB1+qncfFeykWoXMRCo=";
-      };
+  config = lib.mkIf cfg.enable {
+    programs.spicetify = {
+      enable = true;
+
+      theme =
+        let
+          spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+        in
+        spicePkgs.themes.catppuccin
+        // {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "spicetify";
+            rev = "d3c4b697f1739149684e36977a1502f88c344b3a";
+            sha256 = "sha256-VxkgW9qF1pmKnP7Ei7gobF0jVB1+qncfFeykWoXMRCo=";
+          };
+        };
+      colorScheme = ctp.flavor;
     };
-    colorScheme = ctp.flavor;
   };
 }
