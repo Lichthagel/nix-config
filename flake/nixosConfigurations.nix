44e1d4a04c148863e2d63136db636b640fd61d39
Lichthagel <hey@lichthagel.de>
2024-04-08 14:30:35 +0200

    flake: pass self' & inputs' in module args

--- flake/nixosConfigurations.nix
+++ flake/nixosConfigurations.nix
@@ -1,6 +1,7 @@
 {
   self,
   inputs,
+  withSystem,
   lib,
   ...
 }:
@@ -9,24 +10,34 @@
     nixosConfigurations =
       let
         ctpBase = (builtins.fromTOML (builtins.readFile (self + /config.toml))).catppuccin;
-        mkArgs = system: {
-          inherit self inputs;
-          selfPkgs = self.packages.${system};
-          unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${system};
-          ctp = rec {
-            inherit (ctpBase) accent flavor;
+        mkArgs =
+          system:
+          withSystem system (
+            { self', inputs', ... }:
+            {
+              inherit
+                self
+                inputs
+                self'
+                inputs'
+                ;
+              selfPkgs = self'.packages;
+              unstablePkgs = inputs'.nixpkgs-unstable.legacyPackages;
+              ctp = rec {
+                inherit (ctpBase) accent flavor;
 
-            accentCapitalized = palette.${flavor}.name;
-            flavorCapitalized = palette.${flavor}.colors.${accent}.name;
+                accentCapitalized = palette.${flavor}.name;
+                flavorCapitalized = palette.${flavor}.colors.${accent}.name;
 
-            palette = lib.importJSON (
-              builtins.fetchurl {
-                url = "https://raw.githubusercontent.com/catppuccin/palette/563cdbccc813ae6716ef8242391e6f9dca8d7596/palette.json";
-                sha256 = "sha256:1vzg1x2f1j869ggpsjixi4wdw58zxv8641d04vv33ijmdj1d5p8c";
-              }
-            );
-          };
-        };
+                palette = lib.importJSON (
+                  builtins.fetchurl {
+                    url = "https://raw.githubusercontent.com/catppuccin/palette/563cdbccc813ae6716ef8242391e6f9dca8d7596/palette.json";
+                    sha256 = "sha256:1vzg1x2f1j869ggpsjixi4wdw58zxv8641d04vv33ijmdj1d5p8c";
+                  }
+                );
+              };
+            }
+          );
         mkHost =
           { hostName, system }:
           lib.nixosSystem {
