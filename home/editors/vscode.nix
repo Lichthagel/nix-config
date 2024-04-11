6a3611f4dd43cd9905ca7e8825f0e2e2055bf2d6
Lichthagel <hey@lichthagel.de>
2024-04-11 20:22:48 +0200

    vscode: 1.88.0 -> 1.88.1

--- home/editors/vscode.nix
+++ home/editors/vscode.nix
@@ -29,12 +29,12 @@ in
               }
             )).overrideAttrs
             rec {
-              version = "1.88.0";
+              version = "1.88.1";
 
               src = pkgs.fetchurl {
                 name = "VSCode_${version}_linux-x64.tar.gz";
                 url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
-                sha256 = "sha256-rq8WUApP3LDbhV9gvovK20o2QbZikHwhnDf7JdcdKE8=";
+                sha256 = "sha256-YnKo5jTWiulc8tmNm0XPizb+TfSGNNKuIuCEN/nhqZI=";
               };
             }
           ).fhs
