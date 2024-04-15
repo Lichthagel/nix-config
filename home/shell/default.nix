5a1334254edeb7d06e28031ec6bc8b2c561a625a
Lichthagel <hey@lichthagel.de>
2024-04-15 19:11:32 +0200

    home/fzf: use catppuccin-nix

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -45,21 +45,8 @@
 
     fzf = {
       enable = true;
+      catppuccin.enable = true;
       defaultCommand = "fd --type f --hidden --follow --exclude .git";
-      colors = {
-        "bg+" = "#363a4f";
-        bg = "#24273a";
-        spinner = "#f4dbd6";
-        hl = "#ed8796";
-        fg = "#cad3f5";
-        header = "#ed8796";
-        info = "#c6a0f6";
-        pointer = "#f4dbd6";
-        marker = "#f4dbd6";
-        "fg+" = "#cad3f5";
-        prompt = "#c6a0f6";
-        "hl+" = "#ed8796";
-      };
     };
 
     ripgrep = {
