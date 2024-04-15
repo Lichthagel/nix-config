94a7c26553781fc9565b900d09dd07ab36946a54
Lichthagel <hey@lichthagel.de>
2024-04-15 19:11:20 +0200

    home/hyprland: use some catppuccin

--- home/graphical/hyprland.nix
+++ home/graphical/hyprland.nix
@@ -33,6 +33,7 @@ in
 
     wayland.windowManager.hyprland = {
       enable = true;
+      catppuccin.enable = true;
 
       plugins = lib.optional cfg.perMonitorWorkspaces (
         inputs'.split-monitor-workspaces.packages.split-monitor-workspaces.overrideAttrs (oldAttrs: {
@@ -94,8 +95,8 @@ in
           gaps_in = 5;
           gaps_out = 20;
           border_size = 2;
-          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-          "col.inactive_border" = "rgba(595959aa)";
+          "col.active_border" = "rgba($pinkAlphaee) rgba($tealAlphaee) 45deg";
+          "col.inactive_border" = "rgba($crustAlphaaa)";
 
           layout = "dwindle";
 
