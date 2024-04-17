175b575bb2958ef592f9f6c44c3b81656ffddde1
Lichthagel <hey@lichthagel.de>
2024-04-17 13:46:50 +0200

    os/desktop: install nm-openconnect

--- hosts/jdnixos/default.nix
+++ hosts/jdnixos/default.nix
@@ -27,7 +27,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [ networkmanager-openconnect ];
+  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
