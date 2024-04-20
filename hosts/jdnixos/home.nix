8ee1d67167d3f9ef8c83d4d57b72cb68d1c1bc2d
Lichthagel <hey@lichthagel.de>
2024-04-20 16:57:50 +0200

    desktop/hyprland: adjust monitor setup

--- hosts/jdnixos/home.nix
+++ hosts/jdnixos/home.nix
@@ -14,7 +14,10 @@
 
   wayland.windowManager.hyprland.settings = {
     env = [ "WLR_NO_HARDWARE_CURSORS,1" ];
-    monitor = [ "DP-2,3440x1440@144,1920x0,1" "DP-3,1920x1080@60,0x180,1" ];
+    monitor = [
+      "DP-2,3440x1440@144,1920x0,1"
+      "DP-3,1920x1080@60,0x180,1"
+    ];
   };
 
   services.mako.output = "DP-2";
