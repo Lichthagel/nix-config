214c3204028da68b56058ff4f5b48a0a329eb325
Lichthagel <hey@lichthagel.de>
2024-04-24 17:59:32 +0200

    os/desktop/forgejo: setup gitea runner

--- hosts/jdnixos/forgejo.nix
+++ hosts/jdnixos/forgejo.nix
@@ -1,4 +1,9 @@
-{ config, unstablePkgs, ... }:
+{
+  config,
+  lib,
+  unstablePkgs,
+  ...
+}:
 {
   services.forgejo = {
     enable = true;
@@ -18,6 +23,10 @@
         # TODO fetch theme from github
         DEFAULT_THEME = "catppuccin-mocha-green";
         THEMES = "auto,gitea,arc-green,catppuccin-latte-rosewater,catppuccin-latte-flamingo,catppuccin-latte-pink,catppuccin-latte-mauve,catppuccin-latte-red,catppuccin-latte-maroon,catppuccin-latte-peach,catppuccin-latte-yellow,catppuccin-latte-green,catppuccin-latte-teal,catppuccin-latte-sky,catppuccin-latte-sapphire,catppuccin-latte-blue,catppuccin-latte-lavender,catppuccin-frappe-rosewater,catppuccin-frappe-flamingo,catppuccin-frappe-pink,catppuccin-frappe-mauve,catppuccin-frappe-red,catppuccin-frappe-maroon,catppuccin-frappe-peach,catppuccin-frappe-yellow,catppuccin-frappe-green,catppuccin-frappe-teal,catppuccin-frappe-sky,catppuccin-frappe-sapphire,catppuccin-frappe-blue,catppuccin-frappe-lavender,catppuccin-macchiato-rosewater,catppuccin-macchiato-flamingo,catppuccin-macchiato-pink,catppuccin-macchiato-mauve,catppuccin-macchiato-red,catppuccin-macchiato-maroon,catppuccin-macchiato-peach,catppuccin-macchiato-yellow,catppuccin-macchiato-green,catppuccin-macchiato-teal,catppuccin-macchiato-sky,catppuccin-macchiato-sapphire,catppuccin-macchiato-blue,catppuccin-macchiato-lavender,catppuccin-mocha-rosewater,catppuccin-mocha-flamingo,catppuccin-mocha-pink,catppuccin-mocha-mauve,catppuccin-mocha-red,catppuccin-mocha-maroon,catppuccin-mocha-peach,catppuccin-mocha-yellow,catppuccin-mocha-green,catppuccin-mocha-teal,catppuccin-mocha-sky,catppuccin-mocha-sapphire,catppuccin-mocha-blue,catppuccin-mocha-lavender";
+      };
+      actions = {
+        ENABLED = true;
+        DEFAULT_ACTIONS_URL = "https://github.com";
       };
     };
     database = {
@@ -39,9 +48,63 @@
     ensureDatabases = [ "gitea" ];
   };
 
-  sops.secrets.forge_db = {
-    owner = config.services.forgejo.user;
-    group = config.services.forgejo.group;
-    mode = "0600";
+  services.gitea-actions-runner = {
+    instances =
+      let
+        instanceBase = {
+          enable = true;
+          name = "lichthagel-desktop";
+          labels = [
+            "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest"
+            "ubuntu-22.04:docker://gitea/runner-images:ubuntu-22.04"
+            "ubuntu-20.04:docker://gitea/runner-images:ubuntu-20.04"
+          ];
+          settings.container = {
+            network = "bridge";
+          };
+        };
+        mkInstance =
+          args:
+          lib.mkMerge [
+            instanceBase
+            (removeAttrs args [ "tokenSecret" ])
+            (lib.mkIf (args ? tokenSecret) {
+              tokenFile = config.sops.secrets."runner_token/${args.tokenSecret}".path;
+            })
+          ];
+      in
+      {
+        lichtForge = mkInstance {
+          url = "http://desktop.licht:3456";
+          tokenSecret = "forge";
+        };
+        gitea = mkInstance {
+          url = "https://gitea.com";
+          tokenSecret = "gitea";
+        };
+        codeberg = mkInstance {
+          url = "https://codeberg.org";
+          tokenSecret = "codeberg";
+        };
+      };
   };
+
+  sops.secrets =
+    let
+      runnerConfig = {
+        owner = "gitea-runner";
+        group = "gitea-runner";
+        mode = "0600";
+      };
+    in
+    {
+      forge_db = {
+        owner = config.services.forgejo.user;
+        group = config.services.forgejo.group;
+        mode = "0600";
+      };
+      "runner_token/forge" = runnerConfig;
+      "runner_token/gitea" = runnerConfig;
+      "runner_token/codeberg" = runnerConfig;
+    };
 }
