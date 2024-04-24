22a7dffcfb6ee5086b00a0ccb4956215670eeeb2
Lichthagel <hey@lichthagel.de>
2024-04-24 18:56:36 +0200

    home/spotify: use builtin catppuccin theme

--- home/media/spotify.nix
+++ home/media/spotify.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  ctp,
   pkgs,
   inputs,
   ...
@@ -24,15 +23,8 @@ in
       {
         enable = true;
 
-        theme = spicePkgs.themes.catppuccin // {
-          src = pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "spicetify";
-            rev = "d3c4b697f1739149684e36977a1502f88c344b3a";
-            sha256 = "sha256-VxkgW9qF1pmKnP7Ei7gobF0jVB1+qncfFeykWoXMRCo=";
-          };
-        };
-        colorScheme = ctp.flavor;
+        theme = spicePkgs.themes.catppuccin;
+        colorScheme = config.catppuccin.flavour;
 
         enabledExtensions = with spicePkgs.extensions; [ shuffle ];
 
