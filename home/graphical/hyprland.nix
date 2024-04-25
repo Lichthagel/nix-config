597bddbd5e2548886aa497773cad195af356d928
Lichthagel <hey@lichthagel.de>
2024-04-25 17:59:44 +0200

    home/hyprland: anim slide in special vertically

--- home/graphical/hyprland.nix
+++ home/graphical/hyprland.nix
@@ -140,6 +140,7 @@ in
             "borderangle, 1, 8, default"
             "fade, 1, 7, default"
             "workspaces, 1, 6, default"
+            "specialWorkspace, 1, 3, default, slidefadevert"
           ];
         };
 
