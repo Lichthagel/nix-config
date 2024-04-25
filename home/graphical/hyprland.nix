674b9a3740ccb8a9d0e2572f3a908a338075a81f
Lichthagel <hey@lichthagel.de>
2024-04-25 23:27:02 +0200

    home/hyprland: adjust animations

--- home/graphical/hyprland.nix
+++ home/graphical/hyprland.nix
@@ -131,16 +131,16 @@ in
 
           # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          bezier = [ "easeInOutCirc, 0.85, 0, 0.15, 1" ];
 
           animation = [
-            "windows, 1, 7, myBezier"
-            "windowsOut, 1, 7, default, popin 80%"
-            "border, 1, 10, default"
-            "borderangle, 1, 8, default"
-            "fade, 1, 7, default"
+            "windows, 1, 7, easeInOutCirc"
+            "windowsOut, 1, 7, easeInOutCirc, popin 80%"
+            "border, 1, 3, easeInOutCirc"
+            "borderangle, 1, 8, easeInOutCirc"
+            "fade, 1, 7, easeInOutCirc"
             "workspaces, 1, 6, default"
-            "specialWorkspace, 1, 3, default, slidefadevert"
+            "specialWorkspace, 1, 3, easeInOutCirc, slidefadevert"
           ];
         };
 
