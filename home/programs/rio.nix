cc9db87fa90d86da0705280e21388959302cd795
Lichthagel <hey@lichthagel.de>
2024-04-28 00:11:40 +0200

    home/rio: opacity, renderer & fonts

--- home/programs/rio.nix
+++ home/programs/rio.nix
@@ -12,6 +12,25 @@ in
   config = lib.mkIf cfg.enable {
     programs.rio = {
       enable = true;
+      catppuccin.enable = true;
+
+      settings = {
+        window = {
+          foreground-opacity = 1.0;
+          background-opacity = 0.8;
+        };
+
+        renderer = {
+          performance = "High";
+          backend = "Vulkan";
+        };
+
+        fonts = {
+          size = 20;
+          family = "CaskaydiaCove Nerd Font Mono";
+          extra = [ { family = "Symbols Nerd Font"; } ];
+        };
+      };
     };
   };
 }
