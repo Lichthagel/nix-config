48a1f7d599be38f727e116a5bbc56b430b10bcb2
Lichthagel <hey@lichthagel.de>
2024-04-28 23:00:02 +0200

    flake: update inputs

--- nixos/graphical/default.nix
+++ nixos/graphical/default.nix
@@ -35,7 +35,7 @@ in
     environment.sessionVariables = {
       NIXOS_OZONE_WL = "1";
       XDG_SESSION_TYPE = "wayland";
-      QT_QPA_PLATFORM = "wayland";
+      QT_QPA_PLATFORM = "wayland;xcb";
       SDL_VIDEODRIVER = "wayland";
     };
   };
