f3debf7700e6c417b685f68fe708823280b156ab
Lichthagel <hey@lichthagel.de>
2024-05-10 16:29:45 +0200

    home/hyprlock: use home-manager module

--- home/graphical/hyprlock.nix
+++ home/graphical/hyprlock.nix
@@ -3,15 +3,12 @@
   osConfig,
   lib,
   pkgs,
-  inputs,
   ...
 }:
 let
   cfg = config.licht.graphical.hyprlock;
 in
 {
-  imports = [ inputs.hyprlock.homeManagerModules.default ];
-
   options.licht.graphical.hyprlock = {
     enable = lib.mkEnableOption "hyprlock" // {
       default = config.licht.graphical.hyprland.enable;
@@ -33,20 +30,20 @@ in
     programs.hyprlock = {
       enable = true;
 
-      package = pkgs.hyprlock;
+      settings = {
+        general = {
+          grace = 5;
+        };
 
-      general = {
-        grace = 5;
+        background = [
+          {
+            path = "${pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/rose-pine/wallpapers/main/rose_pine_contourline.png";
+              sha256 = "sha256-8OQCXMy27IImp1Oc/X4i14/8k9XjuuU+6clh0rRcAQY=";
+            }}";
+          }
+        ];
       };
-
-      backgrounds = [
-        {
-          path = "${pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/rose-pine/wallpapers/main/rose_pine_contourline.png";
-            sha256 = "sha256-8OQCXMy27IImp1Oc/X4i14/8k9XjuuU+6clh0rRcAQY=";
-          }}";
-        }
-      ];
     };
   };
 }
