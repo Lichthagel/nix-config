439e4eebb0230594302b24db21193ccaca113ef8
Lichthagel <hey@lichthagel.de>
2024-05-23 15:11:38 +0200

    feat(home/atuin): use history filter

--- home/shell/atuin.nix
+++ home/shell/atuin.nix
@@ -5,6 +5,13 @@
     settings = {
       style = "compact";
       enter_accept = true;
+      history_filter = [
+        "^/etc/profiles" # Programs calling programs by full path. E.g. GitLens calling git.
+        "TOKEN="
+        "token="
+        "SECRET="
+        "secret="
+      ];
     };
     package = unstablePkgs.atuin;
   };
