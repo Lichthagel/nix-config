9be59a0d866fce160151fafc8d64bd5ff474eac9
Lichthagel <hey@lichthagel.de>
2024-05-31 00:46:40 +0200

    fix(os): disable systemd-boot editor

--- nixos/default.nix
+++ nixos/default.nix
@@ -18,8 +18,11 @@
 
   config = {
     # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.configurationLimit = 10;
+    boot.loader.systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+      editor = false;
+    };
 
     boot.loader.efi.canTouchEfiVariables = true;
 
