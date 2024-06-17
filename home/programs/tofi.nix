59db3f43b85ad40473b8fc6398d1052c80937603
Lichthagel <hey@lichthagel.de>
2024-06-17 10:17:39 +0200

    feat(home/tofi):  configure

--- home/programs/tofi.nix
+++ home/programs/tofi.nix
@@ -13,6 +13,31 @@ in
     programs.tofi = {
       enable = true;
       catppuccin.enable = true;
+      settings = {
+        font = "CaskaydiaCove Nerd Font";
+        font-features = "calt, ss01";
+
+        text-cursor-style = "bar";
+
+        prompt-text = " ";
+        prompt-padding = 20;
+        num-results = 0;
+        result-spacing = 5;
+
+        width = 1280;
+        height = 720;
+        outline-width = 0;
+        border-width = 0;
+        padding-top = 20;
+        padding-bottom = 20;
+        padding-left = 20;
+        padding-right = 20;
+        clip-to-padding = true;
+
+        hide-cursor = true;
+        text-cursor = true;
+        matching-algorithm = "fuzzy";
+      };
     };
   };
 }
