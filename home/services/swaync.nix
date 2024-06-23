ee5bdbd2e023a3703655656a9bfe179394b7ac95
Lichthagel <hey@lichthagel.de>
2024-06-23 16:06:20 +0200

    fix(home/swaync): attrs for ctp theme hashes

--- home/services/swaync.nix
+++ home/services/swaync.nix
@@ -32,7 +32,14 @@ in
       style = pkgs.substitute {
         src = pkgs.fetchurl {
           url = "https://github.com/catppuccin/swaync/releases/download/v0.2.2/${config.catppuccin.flavor}.css";
-          sha256 = "sha256-YFboTWj/hiJhmnMbGLtfcxKxvIpJxUCSVl2DgfpglfE=";
+          sha256 =
+            let
+              hashes = {
+                frappe = "sha256-/Y0FqRbun2dFnDHTyUvKYTesjH8f2N1n0ITzGR3PlWU=";
+                mocha = "sha256-YFboTWj/hiJhmnMbGLtfcxKxvIpJxUCSVl2DgfpglfE=";
+              };
+            in
+            hashes.${config.catppuccin.flavor};
         };
 
         substitutions = [
